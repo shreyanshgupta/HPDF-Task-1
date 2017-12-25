2dcc7b73376c56ea92af1cf6928dc5f12a516f4b
Shreyansh Gupta <shrey.20may@gmail.com>
2017-12-25 23:33:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Home from './components/home.js'
 import './App.css';
 
 class App extends React.Component {
